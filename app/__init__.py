b4d1c5ffe9bebed38a43179213428e16e767ad4b
khurshid <khurshidsk7304@gmail.com>
2025-05-02 22:04:13 +0530

    cors added

--- app/__init__.py
+++ app/__init__.py
@@ -2,12 +2,14 @@ from dotenv import load_dotenv
 from flask import Flask
 from app.routes.process import process_bp
 from app.routes.tools import tools
+from flask_cors import CORS
 
 
 load_dotenv()
 
 def create_app():
     app=Flask(__name__)
+    CORS(app,supports_credentials=True)
     app.register_blueprint(process_bp)
     app.register_blueprint(tools)
 
